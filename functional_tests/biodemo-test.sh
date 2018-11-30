a3d8afc481b353624f946602bf878715320bdc10
Tan Meng Yue <tanmeng.yue@csl.com.au>
2018-11-30 15:42:37 +1100

    More fixes

--- functional_tests/biodemo-test.sh
+++ functional_tests/biodemo-test.sh
@@ -135,7 +135,7 @@ function test_exit_status {
         echo "Actual exit status: $exit_status"
         echo "Expected exit status: $expected_exit_status"
     fi 
-	coverage run ${1/$test_program/$(which $test_program)}
+	eval "coverage run -a ${1/$test_program/$(which $test_program)}"
 }
 
 
