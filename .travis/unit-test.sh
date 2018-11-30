7542a76f26e0098bfcad5fe3d6829ccc562c4913
nicholas <yue.nicholas@gmail.com>
2018-11-30 04:02:05 +0000

    Working coverage

--- .travis/unit-test.sh
+++ .travis/unit-test.sh
@@ -4,10 +4,10 @@ set -e
 errors=0
 
 # Run unit tests
-python biodemo/biodemo_test.py || {
-    echo "'python python/biodemo/biodemo_test.py' failed"
+coverage run -m unittest discover biodemo || {
+    echo "'coverage run -m unittest discover biodemo' failed"
     let errors+=1
-}
+    }
 
 # Check program style
 pylint -E biodemo/*.py || {
---
+++ biodemo/test_biodemo.py
@@ -0,0 +1,1 @@
+biodemo_test.py
